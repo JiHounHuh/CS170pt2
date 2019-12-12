efc0a7089ebcc52f115ecd46fc36199b58f05363
PatrickWumbo <jhuh009@ucr.edu>
2019-12-12 15:42:43 -0800

    backward done

--- proj2.h
+++ proj2.h
@@ -54,18 +54,17 @@ double leaveOneOut(vector<vector<double>> test, vector<int>goodSet)
     cout << "total is: " << total << endl;
     return total;
 }
-
+vector<int> current_set_of_features;
+vector<int> testerSet;
+vector<int> feature_to_add;
+vector<int> master_list;
+double bestAccuracy = 0.0;
+double accuracy = 0.0;
+double bestestAccuracy = 0.0;
+int goodFeature = 0;
 void forward_search(vector<vector<double>> data) {
     cout << "This is feature size: " << data[0].size()-1 << endl;
     cout << "Data size: " << data.size() << endl;
-    double bestAccuracy = 0.0;
-    double accuracy = 0.0;
-    double bestestAccuracy = 0.0;
-    vector<int> current_set_of_features;
-    vector<int> testerSet;
-    vector<int> feature_to_add;
-    vector<int> master_list;
-    int goodFeature = 0;
     master_list.clear();
     for(int i = 1; i < data[0].size(); i++) 
     { // starting from 1, # of features is 10
@@ -99,9 +98,9 @@ void forward_search(vector<vector<double>> data) {
                     cout << "time to update best: " << accuracy << endl; 
                     bestAccuracy = accuracy;
                     goodFeature = j;
-                    if(bestAccuracy > bestestAccuracy) {
-                        master_list = current_set_of_features;
-                    }
+                    // if(bestAccuracy > bestestAccuracy) {
+                    //     master_list = current_set_of_features;
+                    // }
                 }
             }
         }
@@ -134,15 +133,94 @@ void forward_search(vector<vector<double>> data) {
     return;
 }
 
-double backward_search (vector<vector<double>> bork) {
-    for(int i = bork[0].size(); i >= 0; i--){
-        
+double worstAccuracy,badAccuracy;
+vector<int> feature_to_remove;
+int badFeature = 0;
+
+void backward_search (vector<vector<double>> bork) 
+{
+    worstAccuracy = 0.0;
+    bestestAccuracy = 0.0;
+    for(int po = 1; po < bork[0].size(); po++)
+    {
+        current_set_of_features.push_back(po);
+        //cout << po << " ";
     }
+    //cout << endl;
+    cout << "Bork size: " << bork[0].size() << endl;
+    for(int i = bork[0].size()-1; i > 0; i--) 
+    {
+        cout << "On the " << i << "th level of the search tree" << endl;
+        feature_to_add.clear();
+        worstAccuracy = 100.0;
+        bestAccuracy = 0.0;
+        testerSet.clear();
+        for(int j = 1; j < bork[0].size();j++) 
+        {
+            if((find(current_set_of_features.begin(),current_set_of_features.end(),j) != current_set_of_features.end())) 
+            {
+                cout << "--Considering removing the " << j << " feature" << endl;
+                testerSet = current_set_of_features;
+                testerSet.erase(remove(testerSet.begin(),testerSet.end(),j), testerSet.end());
+                cout << "Tester Set: ";
+                for(int yelp = 0; yelp < testerSet.size();yelp++) { // output tester set
+                    cout << testerSet[yelp] << " ";
+                }
+                cout << endl << "current set: ";
+                for (int kek = 0; kek < current_set_of_features.size();kek++) // output current set of features 
+                {
+                    cout << current_set_of_features[kek] << " ";
+                }
+                cout << endl;
+                accuracy = leaveOneOut(bork,testerSet);
+                cout << "Accuracy: " << accuracy << endl;
+                if(accuracy > bestAccuracy)
+                {
+                    cout << "time to update best: " << accuracy << endl;
+                    bestAccuracy = accuracy;
+                    badFeature = j;
+                }
+                // if(accuracy > badAccuracy) 
+                // {
+                //     badAccuracy = accuracy;
+                //     badFeature = j;
+                // }
+            }
+        }
+        current_set_of_features.erase(remove(current_set_of_features.begin(),current_set_of_features.end(),badFeature),current_set_of_features.end());
+        //cout <<"current features: ";
+        testerSet.clear();
+        if(bestestAccuracy < bestAccuracy)
+        {
+            bestestAccuracy = accuracy;
+            master_list = current_set_of_features;
+        }
+        cout << "Removing badFeature: " << badFeature << endl;
+
+        cout << "On level " << i << " I removed feature " << badFeature << " from current set." << endl;
+        cout << "The best accuracy is: " << bestestAccuracy << endl;
+        cout << "Current Set: ";
+        for(int master = 0; master < current_set_of_features.size();master++) {
+            if (master != current_set_of_features.size()-1) {
+                cout << current_set_of_features[master] << " ";
+            }
+            else {
+                cout << current_set_of_features[master] << endl;
+            }
+        }
+        cout << "Master List: " << endl;
+        for(int curr = 0; curr < master_list.size(); curr++) {
+            cout << master_list[curr] << " ";
+        }
+    }
+    return;
 }
 
+void custom_search(vector<vector<double>> troop) {
+    return;
+}
 void parse_data() {
     vector<vector<double>> data; // stores all data sets
-    vector< pair<double,double>> vect;
     vector<double> temp;
     vector<double> checkup;
     double lowestVal = 0.0;
@@ -173,6 +251,6 @@ void parse_data() {
         count++;
     }
     inFile.close();
-    forward_search(data);
-
+    //forward_search(data);
+    backward_search(data);
 }
